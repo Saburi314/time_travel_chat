5b4199cfe725c57d32c6625a3f1b27dbaa829e41
Saburi <nnta20yy@docomo.ne.jp>
2025-02-03 00:03:23 +0900

    ホーム画面に有名人のラジオボタンを追加

--- src/resources/views/home.blade.php
+++ src/resources/views/home.blade.php
@@ -5,11 +5,31 @@
 @section('content')
 <div class="text-center">
     <div class="mb-4">
-
-    <!-- TODO ここにアプリの説明を記載する -->
-        <p class="lead">このアプリでは、AIとディベートを行うことができます。ディベートのテーマを決めて、AIとの対話を通じて新しい視点を見つけましょう。</p>
-        <p class="lead">ディベートする」ボタンを押して、さっそく始めましょう！</p>
+        <p class="lead">このアプリでは、AIとディベートを行うことができます。</p>
+        <p class="lead">議論相手を選んで、さっそく始めましょう！</p>
     </div>
-    <a href="{{ url('/debate') }}" class="btn btn-success btn-lg">議論する</a>
+
+    <!-- 議論相手の選択 -->
+    <form id="opponent-form" action="{{ url('/debate') }}" method="GET">
+        @php
+            $opponents = [
+                'hiroyuki' => '西村博之',
+                'matsuko' => 'マツコ・デラックス',
+                'takafumi' => '堀江貴文',
+                'toshio' => '村上龍'
+            ];
+        @endphp
+
+        @foreach ($opponents as $key => $name)
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="opponent" id="opponent-{{ $key }}" value="{{ $key }}" {{ $loop->first ? 'checked' : '' }}>
+                <label class="form-check-label" for="opponent-{{ $key }}">
+                    {{ $name }}
+                </label>
+            </div>
+        @endforeach
+
+        <button type="submit" class="btn btn-success btn-lg mt-3">議論する</button>
+    </form>
 </div>
 @endsection
