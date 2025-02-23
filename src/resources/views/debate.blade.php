4ad167792da4eb1c9d86dfbe5c42490ebb5884f9
Saburi <nnta20yy@docomo.ne.jp>
2025-02-23 16:05:44 +0900

    デザイン修正

--- src/resources/views/debate.blade.php
+++ src/resources/views/debate.blade.php
@@ -6,14 +6,14 @@
 <div class="text-center mb-4">
     <img src="{{ asset($opponent->image) }}" alt="{{ $opponent->name }}" class="img-fluid" style="max-width: 200px;">
     <h2>{{ $opponent->name }}</h2>
-    <p class="lead">{{ $opponent->system_message }}</p>
+    <!-- <p class="lead">{{ $opponent->system_message }}</p> -->
 </div>
 
 <div class="chat-container">
     <div id="chat-area" class="border rounded p-3"></div>
 
     <form id="chat-form" class="chat-form">
-        <textarea id="user-input" class="form-control" placeholder="メッセージを入力" rows="2"></textarea>
+        <textarea id="user-input" class="form-control" placeholder="メッセージを入力" rows="1"></textarea>
         <div class="button-container">
             <button id="send-button" type="submit" class="btn btn-success" disabled>送信</button>
             <button id="reset-button" type="button" class="btn btn-danger">会話をリセットする</button>
